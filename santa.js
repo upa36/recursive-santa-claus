0bc794aa32e80eacd55c4a9909315dd8a747087c
John Carmichael <jc@stackeducation.com>
2019-09-04 16:51:47 -0400

    Implemented deliverPresents

--- santa.js
+++ santa.js
@@ -1,5 +1,15 @@
 function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+  if (houses.length === 0) return
+
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+  } else {
+    var firstHalf = houses.slice(0, Math.ceil(houses.length / 2))
+    var secondHalf = houses.slice(Math.ceil(houses.length / 2), houses.length)
+
+    deliverPresents(firstHalf)
+    deliverPresents(secondHalf)
+  }
 }
 
 module.exports = deliverPresents
