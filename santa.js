d516085b0779260831da16d9142eeaa24d5aa92f
Upasana <upasanapaudyal@gmail.com>
2019-09-19 17:36:19 -0400

    Eorked on recursive function

--- santa.js
+++ santa.js
@@ -1,5 +1,15 @@
 function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+  } else {
+    var mid = (parseInt(houses.length)) / 2
+    var firsthalf = houses.slice(0, mid)
+    var secondhalf = houses.slice(mid)
+    deliverPresents(firsthalf)
+    deliverPresents(secondhalf)
+  }
 }
+
 
 module.exports = deliverPresents
